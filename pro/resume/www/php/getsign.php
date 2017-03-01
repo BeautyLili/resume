aff07b2a0ed8d8aa560d93bef6ba3da876f3af94
BeautyLili <1091440259@qq.com>
2017-03-01 17:26:16 +0800

    second submit

--- pro/resume/www/php/getsign.php
+++ pro/resume/www/php/getsign.php
@@ -1,4 +1,5 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 require_once "jssdk.php";
 $jssdk = new JSSDK("wx63dfdadbb6a0dc07", "ac1b6afce607421dd5c62a0124fcaddf");
 $signPackage = $jssdk->GetSignPackage($_POST['url']);
